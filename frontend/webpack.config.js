8f4416b01b898a2320a16e50c7ea595dff9e6aee
anyqx <annieyxu@berkeley.edu>
2021-02-08 17:05:28 -0800

    rails setup

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+module.exports = {
+    context: __dirname,
+    entry: './frontend/super_rental.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env', 'react']
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+}
+
+//Configure your module.rules to use Babel transpilation for:
+// JSX
+// ES6
