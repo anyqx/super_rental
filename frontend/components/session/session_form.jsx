980c8e18e29decb20bb7878f7cf29c51b194606d
anyqx <annieyxu@berkeley.edu>
2021-02-11 11:55:53 -0800

    auth done!

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -5,7 +5,7 @@ class SessionForm extends React.Component {
         super(props);
         this.state = {
             email: '',
-            password: '',
+            password: ''
         }
         this.handleSubmit = this.handleSubmit.bind(this)
     }
@@ -26,37 +26,29 @@ class SessionForm extends React.Component {
         return (
             <ul>
                 {this.props.errors.map((error,i) => (
-                    <li>
+                    <li key={`error-${i}`}>
                         {error}
                     </li>
                 ))}
             </ul>
         )
     }
+
     render() {
         return (
             <div className="login-container">
                 <form onSubmit={this.handleSubmit} className="login-box">
-                    Welcome to Roofstock
+                    Welcome to Roofstock!
           <br />
-          Please {this.props.formType} or {this.props.navLink}
+                Please {this.props.formType} or {this.props.navLink}
                     {this.renderErrors()}
                     <div className="login-form">
                         <br />
                         <label>email:
-              <input type="text"
-                                value={this.state.username}
-                                onChange={this.update('email')}
-                                className="login-input"
-                            />
+                            <input type="text" value={this.state.email} onChange={this.update('email')} className="login-input"/>
                         </label>
                         <br />
-                        <label>Password:
-              <input type="password"
-                                value={this.state.password}
-                                onChange={this.update('password')}
-                                className="login-input"
-                            />
+                        <label>Password: <input type="password" value={this.state.password} onChange={this.update('password')} className="login-input"/>
                         </label>
                         <br />
                         <input className="session-submit" type="submit" value={this.props.formType} />
