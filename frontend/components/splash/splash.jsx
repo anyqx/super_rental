182bbae5ca6f5e41abb78a2622d456d1732926a5
anyqx <annieyxu@berkeley.edu>
2021-02-18 14:18:16 -0800

    splash css

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -19,23 +19,23 @@ class Splash extends React.Component {
             </div>    
 
             {/* why-invest */}
-            <div className='why-invest-box'>
-                <div className='why-text'>Why invest through SuperRental?</div>
+            <div className='section-container'>
+                <div className='section-head-text'>Why invest through SuperRental?</div>
                 <div>
-                    <ul>
+                    <ul className='why-details-container'>
                         <li>
-                            <h2>completed trasnsactions</h2>
-                            <img id='why-invest' src={window.splashWhyInvest1URL} />
-                            <p>More than $2 billion in completed SFR transactions in less than four years</p>
+                            <img id='why-img' src={window.splashWhyInvest1URL} />
+                            <h2>completed transactions</h2>
+                            <p>more than $2 billion in completed SFR transactions in less than four years</p>
                         </li>
                         <li>
+                            <img id='why-img' src={window.splashWhyInvest2URL} />
                             <h2>roofstock guarantee</h2>
-                            <img id='why-invest' src={window.splashWhyInvest2URL} />
-                            <p>Unique 30-day money-back guarantee + guaranteed lease up on vacant properties</p>
+                            <p>unique 30-day money-back guarantee + guaranteed lease up on vacant properties</p>
                         </li>
                         <li>
+                            <img id='why-img' src={window.splashWhyInvest3URL} />
                             <h2>technology advantage</h2>
-                            <img id='why-invest' src={window.splashWhyInvest3URL} />
                             <p>make investment decisions using insights, proprietary data and technology</p>
                         </li>
                     </ul>
@@ -49,27 +49,27 @@ class Splash extends React.Component {
             </div>
 
             {/* why sfr */}
-            <div className='why-sfr-box'>
-                <h1>Why single-family rentals(SFR)</h1>
-                <h2>Provides strong, stable cash flows even through economic cycles</h2>
-                <ul>
-                    <li><img id='why-sfh' src={window.splashWhySfh1URL} />sfr market</li>
-                    <li><img id='why-sfh' src={window.splashWhySfh2URL} />current sfrs in us</li>
-                    <li><img id='why-sfh' src={window.splashWhySfh3URL} />new rental households by 2030</li>
+            <div className='section-container'>
+                <div className='section-head-text'>Why single-family rentals(SFR)</div>
+                    <h2>Provides strong, stable cash flows even through economic cycles</h2>
+                <ul className='why-details-container'>
+                    <li><img id='why-img' src={window.splashWhySfh1URL} />sfr market</li>
+                    <li><img id='why-img' src={window.splashWhySfh2URL} />current sfrs in us</li>
+                    <li><img id='why-img' src={window.splashWhySfh3URL} />new rental households by 2030</li>
                 </ul>
-                <Link className='link-box'>learn more</Link>
             </div>
 
-            <div className='meet-investors'>
-                <h1>Annie Xu</h1>
+            <div className='section-container'>
+                <div className='section-head-text'>Meet our investor/site creator</div>
+                <p>Annie Xu</p>
                 <p> intro </p>
 
             </div>
 
 
             {/* story box */}
-            <div className='story-box'>
-                <h1>Our story</h1>
+            <div className='section-container'>
+                <div className='section-head-text'>Our Story</div>
                 <h2>Founded by pioneers in the single-family rental space, 
                     SuperRental has assembled a world-class team of real estate 
                     and technology professionals committed to a clear mission:</h2>
