41506285357ca2e2173d6af7b870183070ec3452
anyqx <annieyxu@berkeley.edu>
2021-02-17 17:14:10 -0800

    seed update

--- frontend/components/property/property_index_container.js
+++ frontend/components/property/property_index_container.js
@@ -8,7 +8,7 @@ const mapStateToProps = (state, ownProps) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-    fetchProperties: () => dispatch(fetchProperties)
+    fetchProperties: () => dispatch(fetchProperties())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(BenchIndex)
