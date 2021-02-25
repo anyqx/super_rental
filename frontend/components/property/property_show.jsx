442f29686fbe4de4b169d40829400957630a65dc
anyqx <annieyxu@berkeley.edu>
2021-02-24 21:08:04 -0800

    property show

--- frontend/components/property/property_show.jsx
+++ frontend/components/property/property_show.jsx
@@ -1,7 +1,7 @@
 class PropertyShow extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { }
     }
     render() { 
         return null;
